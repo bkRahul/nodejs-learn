e6fc91cc920d664b3a16974f48d0cd1309e1a793
bkRahul <rahulsatya1068@gmail.com>
2020-07-15 12:57:22 +0530

    added function to create directory

---
+++ utils/util.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const fs = require("fs");
+
+//get root folder directory by using filepath of the entry module
+const rootDir = path.dirname(process.mainModule.filename);
+
+const createDirectory = (directory) => {
+  return new Promise((resolve, reject) => {
+    fs.exists(directory, (exist) => {
+      if (!exist) {
+        fs.mkdir(directory, (err) => {
+          if (err) {
+            console.log("directory could not be created");
+            reject(err);
+          } else {
+            resolve("directory created successfully");
+          }
+        });
+      } else {
+        resolve("directory already created");
+      }
+    });
+  });
+};
+
+module.exports.rootDir = rootDir;
+module.exports.createDirectory = createDirectory;
