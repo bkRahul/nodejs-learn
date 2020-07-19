531206e7fb5ec6eebeda29c0bf610e18e40be758
bkRahul <rahulsatya1068@gmail.com>
2020-07-19 17:26:13 +0530

    added route to delete cart item

--- routes/shop.js
+++ routes/shop.js
@@ -14,6 +14,8 @@ router.get("/cart", shopController.getCart);
 
 router.post("/cart", shopController.postCart);
 
+router.post("/delete-cart-item", shopController.deleteCartItem);
+
 router.get("/checkout", shopController.getCheckout);
 
 module.exports = router;
