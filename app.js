8d05098d1b49088d32ea0b045c1682ded623b9e8
bkRahul <rahulsatya1068@gmail.com>
2020-07-25 20:30:06 +0530

    created a user instance

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 const errorController = require("./controllers/error");
-const mongoConnect = require("./utils/database").mongoConnect;
+const database = require("./utils/database");
+const User = require("./models/user");
 
 const app = express();
 
@@ -20,6 +22,42 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
+//get the user id and assign to req.user
+app.use((req, res, next) => {
+  //get database
+  const db = database.getDb();
+  //get collectioon
+  const op = db.collection("users");
+  //find all users
+  op.find()
+    .toArray()
+    .then((user) => {
+      if (user.length > 0) {
+        //create a new user instance
+        req.user = new User(
+          user[0]._id,
+          user[0].username,
+          user[0].email,
+          user[0].cart
+        );
+        console.log("user exists");
+        console.log("req.user===>", req.user);
+        next();
+      } else {
+        op.insertOne({
+          username: "Rahul",
+          email: "test@test.com",
+          cart: { items: [] },
+        });
+        console.log("users collection is created");
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 //group routes across files based on the business logic or url
 app.use("/admin", adminRoutes.routes);
 
@@ -28,7 +66,7 @@ app.use(shopRoutes);
 //a 404 page for non matching routes
 app.use(errorController.get404);
 
-mongoConnect(() => {
+database.mongoConnect(() => {
   app.listen(3001, () => {
     console.log("server is running");
   });
